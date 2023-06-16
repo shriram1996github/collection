1abfedb9f607f9272755818d387eab0206f7f8ad
Shriram <shriram.doifode1996@gmail.com>
2023-06-16 19:02:20 +0530

    Vishal first commit

---
+++ src/com/collection/Arrrrrr.java
@@ -0,0 +1,13 @@
+package com.collection;
+
+public class Arrrrrr {
+
+	public static void main(String[] args) {
+     
+		int arr [] = new int [5];
+		Object arr1 [] = {1,2,"Akshay"};
+		System.out.println(arr1);
+		
+	}
+
+}
